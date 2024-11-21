6e49b936299456b6d80fa5d87f5c13ec10f332d8
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2024-11-21 12:42:17 +0100

    add pages home and movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Movies from "../pages/Movies/Movies";
+import NotFound from "../pages/NotFound/NotFound";
+import Header from "./Header/Header";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./Header.module.css";
+import clsx from "clsx";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
