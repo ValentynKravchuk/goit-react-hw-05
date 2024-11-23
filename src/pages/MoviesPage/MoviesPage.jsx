d07783d4c50163c4f70b037a6bb62e874acdaae6
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2024-11-23 15:12:04 +0100

    add searchParams

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { searchMovies } from "../../service/apiMovies";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import MovieList from "../../components/MovieList/MovieList";
+import { useSearchParams } from "react-router-dom";
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") ?? "";
 
-  const handleSearch = async (query) => {
-    const results = await searchMovies(query);
-    setMovies(results || []);
+  const handleSearch = (newValue) => {
+    setSearchParams({ query: newValue });
   };
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (query) {
+        const results = await searchMovies(query);
+        setMovies(results || []);
+      }
+    };
+    fetchMovies();
+  }, [query]);
 
   return (
     <div>
